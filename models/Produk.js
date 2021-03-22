25f01687903a8dd9104dac390e3c8325bdf30c93
zakiyul <zakiyul.py@gmail.com>
2021-03-22 15:50:17 +0800

    produk schema

---
+++ models/Produk.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const produkSchema = mongoose.Schema({
+  nama: {
+    type: String,
+    required: [true, "Data nama produk belum terisi!"],
+  },
+  harga: {
+    type: Number,
+    required: true,
+  },
+  categoriId: {
+    type: ObjectId,
+    ref: "Categori",
+  },
+  tokoId: {
+    type: ObjectId,
+    ref: "Toko",
+  },
+});
+
+module.exports = mongoose.model("Produk", produkSchema);
