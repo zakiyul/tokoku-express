146023786837c025ce2484dca7a66561524cde42
zakiyul <zakiyul.py@gmail.com>
2021-03-22 15:38:55 +0800

    buat tokoSchema

---
+++ models/Toko.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const tokoSchema = new mongoose.Schema({
+  nama: {
+    type: String,
+    required: [true, "Data nama toko belum terisi!"],
+  },
+  owner: {
+    type: String,
+    required: [true, "Data owner belum terisi!"],
+  },
+  alamat: {
+    type: Text,
+    required: [true, "Data alamat belum terisi!"],
+  },
+});
+
+module.exports = mongoose.model("Toko", tokoSchema);
