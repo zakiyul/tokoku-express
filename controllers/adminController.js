afa6f6a49d0363dca237d9d04e23a56d456f8101
zakiyul <zakiyul.py@gmail.com>
2021-03-24 19:25:37 +0800

    create data categori

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,5 @@
+const Categori = require("../models/Categori");
+
 module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard");
@@ -5,9 +7,17 @@ module.exports = {
   viewToko: (req, res) => {
     res.render("admin/toko/view_toko");
   },
+
   viewCategori: (req, res) => {
     res.render("admin/categori/view_categori");
   },
+  addCategori: async (req, res) => {
+    const { jenis } = req.body;
+    // console.log(jenis);
+    await Categori.create({ jenis: jenis });
+    res.redirect("/categori");
+  },
+
   viewProduk: (req, res) => {
     res.render("admin/produk/view_produk");
   },
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const adminController = require("../controllers/adminController");
 router.get("/", adminController.viewDashboard);
 router.get("/toko", adminController.viewToko);
 router.get("/categori", adminController.viewCategori);
+router.post("/categori", adminController.addCategori);
 router.get("/produk", adminController.viewProduk);
 
 module.exports = router;
